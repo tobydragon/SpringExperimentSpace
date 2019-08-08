07d641919c594c7992e40abd07fb49d311672934
John Hunter <jhunter1@ithaca.edu>
2019-08-08 16:20:39 -0400

    Flushed out ResponsePoolDatastoreTest

--- src/test/java/hello/ResponsePoolDatastoreTest.java
+++ src/test/java/hello/ResponsePoolDatastoreTest.java
@@ -1,10 +1,21 @@
 package hello;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ResponsePoolDatastoreTest {
 
     //Implementations of ResponsePoolDatastore will call this in their tests
-    public static void responsePoolDatastoreTest(ResponsePoolDatastore responsePoolDatastore){
+    public static void responsePoolDatastoreTest(ResponsePoolDatastore responsePoolDatastore) throws IOException {
+        //this test assumes that the datastore starts with 3 responses
+        assertEquals(3, responsePoolDatastore.getAllResponses().size());
 
+        responsePoolDatastore.addResponse(new Response("This is a new response", System.currentTimeMillis()));
+        assertEquals(4, responsePoolDatastore.getAllResponses().size());
+
+        responsePoolDatastore.removeResponse("This is a new response");
+        assertEquals(3, responsePoolDatastore.getAllResponses().size());
     }
 
 }
